12582587790e904a996058f49868d8ccc603649c
Eliut González <63687573+eliutgon@users.noreply.github.com>
2021-10-13 11:10:37 +0200

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,3 @@
 <template>
   <Nuxt />
 </template>
-
-<script>
-export default {
-  head() {
-    const i18nHead = this.$nuxtI18nHead({ addSeoAttributes: true })
-    return {
-      title: `${this.$t('welcome')} / eliut.space`,
-      meta: [...i18nHead.meta],
-      link: [...i18nHead.link]
-    }
-  },
-}
-</script>
